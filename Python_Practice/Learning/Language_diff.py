043834c3eaaa0a8351bb6df6a770ae5fadb2eea8
Ho Tan Dat <htandat@tma.com.vn>
2020-06-10 15:52:40 +0700

    update tree

--- Python_Practice/Learning/Language_diff.py
+++
@@ -1,4 +0,0 @@
-def divise(a,b):
-    return a//b
-
-print (divise(a=15,b=-4))
--- Python_Practice/Learning/splitDigit.py
+++
@@ -1,5 +0,0 @@
-numb = 123456
-numb_to_text = str(numb)
-here = 2
-a = numb_to_text[:here] + numb_to_text[here+1:]
-print(a)
