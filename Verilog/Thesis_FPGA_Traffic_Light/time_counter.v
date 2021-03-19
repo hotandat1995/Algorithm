6e0ca0919ee6c4598f66a73b1aa2a7dcf9bd9171
Dat <51106490+hotandat1995@users.noreply.github.com>
2021-03-19 10:59:46 +0700

    Update time_counter.v
    Update comment for time_counter block

--- Verilog/Thesis_FPGA_Traffic_Light/time_counter.v
+++ Verilog/Thesis_FPGA_Traffic_Light/time_counter.v
@@ -1,7 +1,7 @@
 
 module time_counter
 (
-    /*AUTOARG*/
+    /* AUTO ARG */
     // Outputs
     g_end,
     y_end,
@@ -14,44 +14,47 @@ module time_counter
     fsm_y
 );
 
-// Dinh nghia
-parameter GREEN_TIME  = 15;     ///< Thoi gian cua den xanh
-parameter YELLOW_TIME = 5;      ///< Thoi gian cua den vang
-parameter RED_TIME    = 2;     ///< Thoi gian cua den do
+    // Dinh nghia
+    parameter GREEN_TIME  = 15;     ///< Thoi gian cua den xanh
+    parameter YELLOW_TIME = 5;      ///< Thoi gian cua den vang
+    parameter RED_TIME    = 2;      ///< Thoi gian cua den do
 
-/* Interface */
-// INPUT
-input       clk;
-input       rst_n;
-input       fsm_g;
-input       fsm_r;
-input       fsm_y;
-// OUTPUT
-output wire g_end;
-output wire y_end;
-output wire r_end;
+    /* Interface */
+    // INPUT
+    input       clk;
+    input       rst_n;
+    input       fsm_g;
+    input       fsm_r;
+    input       fsm_y;
+    // OUTPUT
+    output wire g_end;
+    output wire y_end;
+    output wire r_end;
 
-// Internal signals //
-reg  [7:0]  clk_counter;        ///< Giu gia tri cua counter
-wire        clr_counter;        ///< Clear counter
+    // Internal signals //
+    reg  [7:0]  clk_counter;        ///< Giu gia tri cua counter
+    wire        clr_counter;        ///< Clear counter
 
-// posedge: positive edge -> Canh len
-always @ (posedge clk)
-begin
-    if (~rst_n)                                         ///< Neu co reset
-        clk_counter[7:0] <= 8'd0;                       ///< Gan gia tri cua counter ve 0
-    else if (clr_counter)                               ///< Neu co clear counter
-        clk_counter[7:0] <= 8'd0;                       ///< Gan gia tri cua counter ve 0
-    else                                                ///< Truong hop con lai
-        clk_counter[7:0] <= clk_counter[7:0] + 1'b1;    ///< Tang gia tri counter len 1 don vi
-end
+    // posedge: positive edge -> Canh len
+    always @ (posedge clk)
+    begin
+        if (~rst_n)                                         ///< Neu co reset
+            clk_counter[7:0] <= 8'd0;                       ///< Gan gia tri cua counter ve 0
+        else if (clr_counter)                               ///< Neu co clear counter
+            clk_counter[7:0] <= 8'd0;                       ///< Gan gia tri cua counter ve 0
+        else                                                ///< Truong hop con lai
+            clk_counter[7:0] <= clk_counter[7:0] + 1'b1;    ///< Tang gia tri counter len 1 don vi
+    end
 
-// Compare the end time (So sanh thoi gian ket thuc)
-assign g_end = fsm_g & (clk_counter[7:0] == GREEN_TIME);
-assign y_end = fsm_y & (clk_counter[7:0] == YELLOW_TIME);
-assign r_end = fsm_r & (clk_counter[7:0] == RED_TIME);
+    // So sanh thoi gian ket thuc cua tung den
+    /* Cac chan fsm_g, fsm_y, fsm_r cho biet trang thai hien tai la den nao dang chay o khoi FSM.
+     * Nen khi clk_counter tang len bang voi gia tri da dinh + fsm_* pin phai dang o trang thai ON (1).
+     * Sau do pin *_end se duoc update len ON (1) de thong bao la da dem du thoi gian cho den tuong ung. */
+    assign g_end = fsm_g & (clk_counter[7:0] == GREEN_TIME);
+    assign y_end = fsm_y & (clk_counter[7:0] == YELLOW_TIME);
+    assign r_end = fsm_r & (clk_counter[7:0] == RED_TIME);
 
-// Clear counter trong trong truong hop 1 trong 3 tin hieu ben duoi == 1
-assign clr_counter = g_end | y_end | r_end;
+    // Clear counter trong trong truong hop 1 trong 3 tin hieu ben duoi == 1
+    assign clr_counter = g_end | y_end | r_end;
 
 endmodule
